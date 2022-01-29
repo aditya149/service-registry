2c8b10c320ce2de05af97479bcdf4eb0805cad9e
Aditya <adityajagtap149@gmail.com>
2022-01-29 17:39:07 +0530

    first commit

---
+++ src/main/java/com/cytrogene/service/registry/ServiceRegistryApplication.java
@@ -0,0 +1,15 @@
+package com.cytrogene.service.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //This is important to start eureka server.
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+
+}
